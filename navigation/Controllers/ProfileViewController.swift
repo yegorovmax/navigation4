0f5d2162c7ee17d9f6d9eeffd529bb75b832aec4
max_egorov <max.egorov@sureprep.com>
2022-02-22 19:11:26 -0800

    updated HW

--- navigation/Controllers/ProfileViewController.swift
+++ navigation/Controllers/ProfileViewController.swift
@@ -9,27 +9,33 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
-    let profileHeaderView = ProfileHeaderView()
+    private lazy var profileHeaderView: ProfileHeaderView = {
+        let view = ProfileHeaderView(frame: .zero)
+        view.delegate = self
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
     
     private var heightConstraint: NSLayoutConstraint?
     
-//    private lazy var profileHeaderView: ProfileHeaderView = {
-//        let view = ProfileHeaderView(frame: .zero)
-//        view.delegate = self
-//        view.translatesAutoresizingMaskIntoConstraints = false
-//        return view
-//    }()
-
-    
     override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.addSubview(self.profileHeaderView)
+        setupView()
         let tap = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
         view.addGestureRecognizer(tap)
-        super.viewDidLoad()
-        view.backgroundColor = .lightGray
-        view.addSubview(profileHeaderView)
-        self.setupNaigationBar()
-        //self.setupView()
-        
+    }
+    
+    private func setupView() {
+        self.view.backgroundColor = .lightGray
+        let viewTopConstraint = self.profileHeaderView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor)
+        let viewLeadingConstraint = self.profileHeaderView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor)
+        let viewTrailingConstraint = self.profileHeaderView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor)
+        self.heightConstraint = self.profileHeaderView.heightAnchor.constraint(equalToConstant: 220)
+        NSLayoutConstraint.activate([
+            viewTopConstraint, viewLeadingConstraint, viewTrailingConstraint, self.heightConstraint
+        ].compactMap( {$0} )) 
     }
     
     internal override func viewWillLayoutSubviews() {
@@ -37,18 +43,7 @@ class ProfileViewController: UIViewController {
         profileHeaderView.frame = CGRect(origin: CGPoint.zero, size: view.frame.size)
     }
 
-//    private func setupView() {
-//        self.view.backgroundColor = .red
-//        self.view.addSubview(self.profileHeaderView)
-//        let topConstraint = self.profileHeaderView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor)
-//        let leadingConstraint = self.profileHeaderView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor)
-//        let trailingConstraint = self.profileHeaderView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor)
-//        self.heightConstraint = self.profileHeaderView.heightAnchor.constraint(equalToConstant: 170)
-//
-//        NSLayoutConstraint.activate([
-//            topConstraint, leadingConstraint, trailingConstraint, self.heightConstraint
-//        ].compactMap({ $0 }))
-//    }
+    
     private func setupNaigationBar() {
         let screenSize: CGRect = UIScreen.main.bounds
         let navBar = UINavigationBar(frame: CGRect(x: 0, y: 35, width: screenSize.width, height: 100))
@@ -60,10 +55,9 @@ class ProfileViewController: UIViewController {
 }
 extension ProfileViewController: ProfileHeaderViewProtocol {
     
-    func didTapStatusButton(textFieldIsVisible: Bool, completion: @escaping () -> Void) {
-        self.heightConstraint?.constant = textFieldIsVisible ? 214 : 170
-        
-        UIView.animate(withDuration: 0.3, delay: 0.0) {
+    func buttonAction(inputTextIsVisible: Bool, completion: @escaping () -> Void) {
+        self.heightConstraint?.constant = inputTextIsVisible ? 250 : 220
+        UIView.animate(withDuration: 0.2, delay: 0.0) {
             self.view.layoutIfNeeded()
         } completion: { _ in
             completion()
--- navigation/Controllers/TabBarController.swift
+++ navigation/Controllers/TabBarController.swift
@@ -22,12 +22,12 @@ class TabBarController: UITabBarController {
             }
         }
         
-        var image: UIImage? {
+        var image: String {
             switch self {
             case .feed:
-                return UIImage(systemName: "house")
+                return "house"
             case .profile:
-                return UIImage(systemName: "person.circle")
+                return "person.circle"
             }
         }
     }
@@ -36,21 +36,33 @@ class TabBarController: UITabBarController {
         super.viewDidLoad()
         self.setupTabBar()
     }
-    
+   
     private func setupTabBar() {
-        let items: [TabBarItem] = [.feed, .profile]
-        
-        self.viewControllers = items.map({ tabBarIten in
-            switch tabBarIten {
+      
+        let dataSource: [TabBarItem] = [.feed, .profile]
+       
+        self.viewControllers = dataSource.map {
+            switch $0 {
             case .feed:
-                return UINavigationController(rootViewController: FeedViewController())
+                let feedViewController = FeedViewController()
+                feedViewController.title = TabBarItem.feed.title
+                return self.wrappedInNavigationController(with: FeedViewController(), title: $0.title)
             case .profile:
-                return ProfileViewController()
+                let profileViewController = ProfileViewController()
+                profileViewController.title = TabBarItem.profile.title
+                return self.wrappedInNavigationController(with: profileViewController, title: $0.title)
             }
-        })
-        self.viewControllers?.enumerated().forEach({ (index, vc) in
-            vc.tabBarItem.title = items[index].title
-            vc.tabBarItem.image = items[index].image
-        })
+        }
+      
+        self.viewControllers?.enumerated().forEach {
+            $1.tabBarItem.title = dataSource[$0].title
+            $1.tabBarItem.image = UIImage(systemName: dataSource[$0].image)
+            $1.tabBarItem.imageInsets = UIEdgeInsets(top: 5, left: .zero, bottom: -5, right: .zero)
+        }
+    }
+ 
+    private func wrappedInNavigationController(with: UIViewController, title: Any?) -> UINavigationController {
+        return UINavigationController(rootViewController: with)
+        
     }
 }
